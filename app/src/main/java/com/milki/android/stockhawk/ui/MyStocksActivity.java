5bc2fc4b292f87ff091761329948396ffac26058
Milki Minz <wwwmilki.minz096@gmail.com>
2016-08-16 01:44:51 +0530

    Update MyStocksActivity.java

--- app/src/main/java/com/milki/android/stockhawk/ui/MyStocksActivity.java
+++ app/src/main/java/com/milki/android/stockhawk/ui/MyStocksActivity.java
@@ -117,7 +117,7 @@ public class MyStocksActivity extends AppCompatActivity implements LoaderManager
                       new String[] { input.toString() }, null);
                   if (c.getCount() != 0) {
                     Toast toast =
-                        Toast.makeText(MyStocksActivity.this, "This stock is already saved!",
+                        Toast.makeText(MyStocksActivity.this, R.string.already",
                             Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.CENTER, Gravity.CENTER, 0);
                     toast.show();
